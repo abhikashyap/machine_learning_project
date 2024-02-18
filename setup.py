6f8340a77da21bfa0226fe912a6597c3fc371054
abhikashyap <abhikashyap.ds@gmail.com>
2024-02-18 07:52:30 +0530

    using -e .

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+from typing import List
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+def get_requirements_list()->List[str]:
+  """
+  Description: this will return list of requirement in requirements.txt  as a list
+  """
+  with open(REQUIREMENT_FILE_NAME) as requirements_file:
+    return requirements_file.readlines()
+
+
+setup(
+  name="housing-predictor",
+  version="0.0.4",
+  author="abhishek",
+  description="this is first ml project",
+  packages=find_packages(),
+  install_requires=get_requirements_list()
+)
+
+ 
